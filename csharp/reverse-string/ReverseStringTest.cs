304c4de02e9a27ec2badfcc87e3b6f988c70e991
Nathan Stier <tehnatha@nomac.lan>
2018-02-21 15:05:04 -0800

    csharp reverse-string done

--- csharp/reverse-string/ReverseStringTest.cs
+++ csharp/reverse-string/ReverseStringTest.cs
@@ -10,25 +10,25 @@ public class ReverseStringTest
         Assert.Equal("", ReverseString.Reverse(""));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void A_word()
     {
         Assert.Equal("tobor", ReverseString.Reverse("robot"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void A_capitalized_word()
     {
         Assert.Equal("nemaR", ReverseString.Reverse("Ramen"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void A_sentence_with_punctuation()
     {
         Assert.Equal("!yrgnuh m'I", ReverseString.Reverse("I'm hungry!"));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void A_palindrome()
     {
         Assert.Equal("racecar", ReverseString.Reverse("racecar"));
