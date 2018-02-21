ef2e1c18606429719d495536b5b0b611eabd030e
Nathan Stier <tehnatha@nomac.lan>
2018-02-21 15:22:33 -0800

    csharp reverse-string done again

--- csharp/reverse-string/ReverseString.cs
+++ csharp/reverse-string/ReverseString.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using System.Linq;
 
 public static class ReverseString
 {
     public static string Reverse(string input)
     {
-        //throw new NotImplementedException("You need to implement this function.");
         if (string.IsNullOrEmpty(input)) return input;
-        return input.Reverse().Aggregate("", (result, element) => result + element );
+        var work = input.ToCharArray();
+        Array.Reverse(work);
+        return new string(work);
+
     }
 }
