662bf6eeece9b783aa453e5ae0880a8566f6cc8b
Nathan Stier <tehnatha@nomac.lan>
2018-02-21 15:36:38 -0800

    csharp leap done

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -2,8 +2,16 @@ using System;
 
 public static class Leap
 {
+    //on every year that is evenly divisible by 4
+    //  except every year that is evenly divisible by 100
+    //    unless the year is also evenly divisible by 400
     public static bool IsLeapYear(int year)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (year % 4 != 0)
+            return false;
+        else if (year % 100 == 0 && year % 400 != 0)
+            return false;
+        else 
+            return true;
     }
 }
--- csharp/leap/LeapTest.cs
+++ csharp/leap/LeapTest.cs
@@ -10,19 +10,19 @@ public class LeapTest
         Assert.False(Leap.IsLeapYear(2015));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Year_divisible_by_4_not_divisible_by_100_is_leap_year()
     {
         Assert.True(Leap.IsLeapYear(1996));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Year_divisible_by_100_not_divisible_by_400_is_common_year()
     {
         Assert.False(Leap.IsLeapYear(2100));
     }
 
-    [Fact(Skip = "Remove to run test")]
+    [Fact]
     public void Year_divisible_by_400_is_leap_year()
     {
         Assert.True(Leap.IsLeapYear(2000));
