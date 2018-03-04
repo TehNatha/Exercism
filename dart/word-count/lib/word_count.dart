0e89c9912d9e8e44ff0abe87c7b49ec4f05a6869
Nathan Stier <tehnatha@nomac.lan>
2018-03-04 01:49:45 -0800

    dart word-count

--- dart/word-count/lib/word_count.dart
+++ dart/word-count/lib/word_count.dart
@@ -1,3 +1,16 @@
 class WordCount {
-  // Put your code here
+  countWords(String val) {
+    var value = val
+        .toLowerCase()
+        .replaceAll(new RegExp(r"(' | ')"), " ")
+        .replaceAll(new RegExp(r"[.,?!:;""@&@\$%\^]+"), " ")
+        .replaceAll(new RegExp(r"\s+"), " ")
+        .trim();
+    var wordList = value.split(new RegExp(r"\s+"));
+    var wordSet = new Set.from(wordList);
+    var wordMap = new Map.fromIterable(wordSet,
+        key: (word) => word,
+        value: (word) => wordList.where((item) => item == word).length);
+    return wordMap;
+  }
 }
