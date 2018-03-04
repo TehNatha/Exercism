e78555c399d303a6946563f3b39cd1ba32beca91
Nathan Stier <tehnatha@nomac.lan>
2018-03-03 21:15:51 -0800

    dart leap

--- dart/leap/lib/leap.dart
+++ dart/leap/lib/leap.dart
@@ -1,3 +1,9 @@
 class Year {
-  // Put your code here
+  int year;
+  Year(this.year);
+  bool isLeap() {
+    if (year % 4 != 0) return false;
+    if (year % 100 == 0 && year % 400 != 0) return false;
+    return true;
+  }
 }
