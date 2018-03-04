8649c6bd55896dd332c5b0f4100870371855c355
Nathan Stier <tehnatha@nomac.lan>
2018-03-03 21:39:26 -0800

    dart squares

--- dart/difference-of-squares/lib/difference_of_squares.dart
+++ dart/difference-of-squares/lib/difference_of_squares.dart
@@ -1,3 +1,15 @@
 class DifferenceOfSquares {
-  // Put your code here
+  
+  int squareOfSums(int val) {
+    int res = 0;
+    for (int i = 1; i <= val; i++) res += i;
+    return res * res;
+  }
+  
+  int sumOfSquares(int val) {
+    int res = 0;
+    for (int i = 1; i <= val; i++) res += i * i;
+    return res;
+  }
+  difference(int val) => squareOfSums(val) - sumOfSquares(val);
 }
