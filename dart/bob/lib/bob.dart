1d2e71fdd8208bc81d1ad849b480b64317238417
TehNatha <nathan.stier@gmail.com>
2018-03-13 16:18:30 -0700

    dart bob

--- dart/bob/lib/bob.dart
+++ dart/bob/lib/bob.dart
@@ -1,3 +1,13 @@
 class Bob {
-  // Put your code here
+  static Map<String, bool Function(String)> teen = {
+    "Whoa, chill out!":
+      (String phrase) => phrase == phrase.toUpperCase() && phrase.contains(new RegExp(r"[A-Z]")),
+    "Sure.":
+      (String phrase) => phrase.endsWith("?"),
+    "Fine. Be that way!":
+      (String phrase) => phrase.trim().isEmpty
+  };
+  String hey(String phrase) {
+    return teen.keys.firstWhere((response) => teen[response](phrase), orElse: ()=>"Whatever.");
+  }
 }
--- dart/bob/test/bob_test.dart
+++ dart/bob/test/bob_test.dart
@@ -15,55 +15,55 @@ void main() {
       final result = bob.hey("WATCH OUT!");
 
       expect(result, equals("Whoa, chill out!"));
-    }, skip: true);
+    });
 
     test("asking a question", () {
       final result = bob.hey("Does this cryogenic chamber make me look fat?");
 
       expect(result, equals("Sure."));
-    }, skip: true);
+    });
 
     test("talking forcefully", () {
       final result = bob.hey("Let\"s go make out behind the gym!");
 
       expect(result, equals("Whatever."));
-    }, skip: true);
+    });
 
     test("using acronyms in regular speech", () {
       final result = bob.hey("It\"s OK if you don\"t want to go to the DMV.");
 
       expect(result, equals("Whatever."));
-    }, skip: true);
+    });
 
     test("forceful questions", () {
       final result = bob.hey("WHAT THE HELL WERE YOU THINKING?");
 
       expect(result, equals("Whoa, chill out!"));
-    }, skip: true);
+    });
 
     test("shouting numbers", () {
       final result = bob.hey("1, 2, 3 GO!");
 
       expect(result, equals("Whoa, chill out!"));
-    }, skip: true);
+    });
 
     test("only numbers", () {
       final result = bob.hey("1, 2, 3");
 
       expect(result, equals("Whatever."));
-    }, skip: true);
+    });
 
     test("question with only numbers", () {
       final result = bob.hey("4?");
 
       expect(result, equals("Sure."));
-    }, skip: true);
+    });
 
     test("shouting with special characters", () {
       final result = bob.hey("ZOMG THE %^*@#\$(*^ ZOMBIES ARE COMING!!11!!1!");
 
       expect(result, equals("Whoa, chill out!"));
-    }, skip: true);
+    });
 
     test("shouting with umlauts", () {
       /// NOTE:
@@ -79,42 +79,42 @@ void main() {
       final result = bob.hey("\xdcML\xc4\xdcTS!");
 
       expect(result, equals("Whoa, chill out!"));
-    }, skip: true);
+    });
 
     test("calmly speaking about umlauts", () {
       final result = bob.hey("\xfcML\xe4\xdcTS");
 
       expect(result, equals("Whatever."));
-    }, skip: true);
+    });
 
     test("shouting with no exclamation mark", () {
       final result = bob.hey("I HATE YOU");
 
       expect(result, equals("Whoa, chill out!"));
-    }, skip: true);
+    });
 
     test("statement containing question mark", () {
       final result = bob.hey("Ending with a ? means a question.");
 
       expect(result, equals("Whatever."));
-    }, skip: true);
+    });
 
     test("prattling on", () {
       final result = bob.hey("Wait! Hang on.  Are you going to be OK?");
 
       expect(result, equals("Sure."));
-    }, skip: true);
+    });
 
     test("silence", () {
       final result = bob.hey("");
 
       expect(result, equals("Fine. Be that way!"));
-    }, skip: true);
+    });
 
     test("prolonged silence", () {
       final result = bob.hey("   ");
 
       expect(result, equals("Fine. Be that way!"));
-    }, skip: true);
+    });
   });
 }
