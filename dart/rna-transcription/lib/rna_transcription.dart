c1ebb75d80f3d931496c371d61e1c20f5126c2fb
TehNatha <nathan.stier@gmail.com>
2018-03-13 17:58:05 -0700

    dart rna

--- dart/rna-transcription/lib/rna_transcription.dart
+++ dart/rna-transcription/lib/rna_transcription.dart
@@ -1,3 +1,12 @@
 class RnaTranscription {
-  // Put your code here
+  Map<String, String> transcription = {
+    "G":"C",
+    "C":"G",
+    "T":"A",
+    "A":"U"
+  };
+  String toRna (String dna) {
+    if (dna.contains(new RegExp(r"[^GCTA]"))) throw new ArgumentError();
+    return dna.split('').map((x) => transcription[x]).join();
+  }
 }
--- dart/rna-transcription/test/rna_transcription_test.dart
+++ dart/rna-transcription/test/rna_transcription_test.dart
@@ -16,33 +16,33 @@ void main() {
         final result = rnaTranscription.toRna("G");
 
         expect(result, equals("C"));
-      }, skip: true);
+      });
 
       test("transcribes adenine to uracil", () {
         final result = rnaTranscription.toRna("A");
 
         expect(result, equals("U"));
-      }, skip: true);
+      });
 
       test("transcribes thymine to adenine", () {
         final result = rnaTranscription.toRna("T");
 
         expect(result, equals("A"));
-      }, skip: true);
+      });
 
       test("transcribes all dna nucleotides to their rna complements", () {
         final result = rnaTranscription.toRna("ACGTGGTCTTAA");
 
         expect(result, equals("UGCACCAGAAUU"));
-      }, skip: true);
+      });
 
       test("correctly handles completely invalid input", () {
         expect(() => rnaTranscription.toRna("XXX"), throwsArgumentError);
-      }, skip: true);
+      });
 
       test("correctly handles partially invalid input", () {
         expect(() => rnaTranscription.toRna("ACGTXXXCTTAA"), throwsArgumentError);
-      }, skip: true);
+      });
     });
   });
 }
