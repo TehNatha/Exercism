11c09565a20bc031a97a20180d38827cb9fc69ad
TehNatha <nathan.stier@gmail.com>
2018-03-13 17:07:25 -0700

    dart phone

--- dart/phone-number/lib/phone_number.dart
+++ dart/phone-number/lib/phone_number.dart
@@ -1,3 +1,15 @@
 class PhoneNumber {
-  // Put your code here.
+  String cleanNumber(String number) {
+    if (number.contains(new RegExp(r"[!\:@]"))) return null;
+
+    String digits = new RegExp(r"[0-9]")
+      .allMatches(number)
+      .map((item) => item.group(0))
+      .reduce((lhs, rhs) => lhs + rhs);
+
+    if (digits[0] == "1") digits = digits.substring(1);
+    if (digits.length != 10) return null;
+    if (digits[3] == "1" || digits[3] == "0") return null;
+    return digits;
+  }
 }
