b6cfe04365e0e2874bcc9b26e52d9083fb47a0f0
TehNatha <nathan.stier@gmail.com>
2019-08-25 12:51:03 -0700

    solution

--- dart/resistor-color/lib/resistor_color.dart
+++ dart/resistor-color/lib/resistor_color.dart
@@ -1,3 +1,4 @@
 class ResistorColor {
-  // Put your code here
+  static const _codes = const ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
+  int colorCode(String color) => _codes.indexOf(color);
 }
