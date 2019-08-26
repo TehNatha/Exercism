7e0b835c2bf373ffb4f95418f54ac071cf007bd2
TehNatha <nathan.stier@gmail.com>
2019-08-25 21:34:57 -0700

    solution

--- dart/allergies/lib/allergies.dart
+++ dart/allergies/lib/allergies.dart
@@ -1,3 +1,4 @@
 class Allergies {
-  // Put your code here
+  static const _knownAllergens = const ["eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"];
+  bool allergicTo(String allergen, int score) => score & (1 << _knownAllergens.indexOf(allergen)) == 1;
 }
