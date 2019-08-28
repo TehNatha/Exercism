05e8153ef15d290c5a3b6b5908e3fedda226bdd3
TehNatha <nathan.stier@gmail.com>
2019-08-27 21:53:41 -0700

    correct allergies

--- dart/allergies/lib/allergies.dart
+++ dart/allergies/lib/allergies.dart
@@ -1,4 +1,16 @@
 class Allergies {
-  static const _knownAllergens = const ["eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats"];
-  bool allergicTo(String allergen, int score) => score & (1 << _knownAllergens.indexOf(allergen)) == 1;
+  static const Map<String, int> _knownAllergens = const {
+    "eggs" : 1,
+    "peanuts" : 2,
+    "shellfish" : 4,
+    "strawberries" : 8,
+    "tomatoes" : 16,
+    "chocolate" : 32,
+    "pollen" : 64,
+    "cats" : 128
+  };
+  bool allergicTo(String allergen, int score) =>
+    score & _knownAllergens[allergen] == _knownAllergens[allergen];
+  List<String> list(int score) =>
+    _knownAllergens.keys.where((allergen) => allergicTo(allergen, score)).toList();
 }
