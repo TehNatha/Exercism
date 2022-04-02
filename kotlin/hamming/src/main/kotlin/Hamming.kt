c6189e4fa8b859f5152c08bad2c8f190bcad1894
TehNatha <nathan.stier@gmail.com>
2022-04-02 15:17:44 -0700

    Use zip and count

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -1,16 +1,9 @@
 object Hamming {
 
-    fun compute(leftStrand: String, rightStrand: String): Int {
-        require(leftStrand.length == rightStrand.length)
+    fun compute(leftDna: String, rightDna: String): Int {
+        require(leftDna.length == rightDna.length)
             {"left and right strands must be of equal length"}
-        
-        val maxIndex = leftStrand.length - 1
-        var hammingNumber = 0
 
-        for (index in 0..maxIndex)
-            if (leftStrand[index] != rightStrand[index])
-                hammingNumber++
-
-        return hammingNumber
+        return leftDna.zip(rightDna).count { it.first != it.second }
     }
 }
